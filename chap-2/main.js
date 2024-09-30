d089968fbba6dfc210005d13092863339066065b
uroojshakeel <165478020+uroojshakeel@users.noreply.github.com>
2024-10-01 02:15:04 +0500

    Add files via upload

---
+++ chap-2/main.js
@@ -0,0 +1,34 @@
+                // Q1
+//   Write code that declares a variable message, assigns the value "Hello, world!" to it, 
+// and then displays an alert with that message 
+
+var message =("Hello, world!");
+alert(message);
+
+
+               // Q2
+//   Declare a variable named age, leave it undefined, then assign the value 25 to it in a later statement.
+//  Display an alert showing the value of age. 
+var age;
+age=25;
+alert(age);
+
+               // Q3
+ //  Declare a variable named greeting, assign it the value "Good morning!",
+//  and then display it in an alert. Afterwards, change the value of greeting to "Good evening!"
+//  and display the new value in another alert. 
+var greeting = ("Good morning!");
+alert(greeting);
+
+greeting = ("Good evening!");
+alert(greeting);
+
+
+                // Q4
+//  Create a variable called favoriteColor and assign it the value "blue". 
+//  Then display an alert that says "My favorite color is blue" using the variable. 
+
+var favoriteColor=("blue");
+alert("My favorite color is" +" "+ favoriteColor);
+
+  
